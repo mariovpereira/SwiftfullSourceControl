c427ba4e6f172bee87723f6610fc69a9f5e18e29
Mario Vincent Pereira <mario@miamiapplab.com>
2024-05-11 18:14:56 -0400

    Initial Commit

---
+++ SwiftfullSourceControl/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftfullSourceControl
+//
+//  Created by Mario Pereira on 5/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftfullSourceControl/SwiftfullSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfullSourceControlApp.swift
+//  SwiftfullSourceControl
+//
+//  Created by Mario Pereira on 5/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfullSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
