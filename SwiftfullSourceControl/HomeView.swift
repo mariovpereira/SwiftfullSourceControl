79e1017d291aae792444afe014eefeaef68cd8ab
Mario Vincent Pereira <mario@miamiapplab.com>
2024-05-12 10:25:41 -0400

    Add HomeView

---
+++ SwiftfullSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfullSourceControl
+//
+//  Created by Mario Pereira on 5/12/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
