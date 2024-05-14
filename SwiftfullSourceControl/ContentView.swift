b51244cef216c7a6369d1df323a224f1b71bc854
Mario Vincent Pereira <mario@miamiapplab.com>
2024-05-13 21:43:06 -0400

    Auto stash before rebase of "refs/heads/main"

--- SwiftfullSourceControl/ContentView.swift
+++ SwiftfullSourceControl/ContentView.swift
@@ -27,7 +27,9 @@ struct ContentView: View {
             Button("Click Me2!") {
                 print("Hello World")
             }
-            
+            Rectangle()
+                .fill(.red)
+                .frame(width: 100, height: 100)
         }
         .padding()
     }
