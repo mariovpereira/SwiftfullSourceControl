df04cb8dea9afbb15ed21f5ce06e7b5e9e746213
Mario Vincent Pereira <mario@miamiapplab.com>
2024-05-18 10:12:54 -0400

    [Feature] Title to Hello

--- SwiftfullSourceControl/HomeView.swift
+++ SwiftfullSourceControl/HomeView.swift
@@ -8,11 +8,16 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @State private var title: String = "Hello"
+    
     var body: some View {
         VStack {
             Text("Screen2")
             Text("Screen2")
-
+        }
+        .onAppear {
+            //send analytics
         }
     }
 }
