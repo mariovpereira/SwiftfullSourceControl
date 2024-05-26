67d7bd714a8c9521c7dff1102f2c3fe2cc7fb7ef
Mario Vincent Pereira <mario@miamiapplab.com>
2024-05-25 21:09:49 -0400

    [Feature] Add settings view

---
+++ SwiftfullSourceControl/SetingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SetingsView.swift
+//  SwiftfullSourceControl
+//
+//  Created by Mario Pereira on 5/25/24.
+//
+
+import SwiftUI
+
+struct SetingsView: View {
+    var body: some View {
+        Text("Settings View")
+    }
+}
+
+#Preview {
+    SetingsView()
+}
