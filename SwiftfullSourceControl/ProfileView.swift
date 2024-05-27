66b18bdf0ff4847469ff3e0b9c2ef529778a7b6b
Mario Vincent Pereira <mario@miamiapplab.com>
2024-05-27 16:49:00 -0400

    [Feature] Add ProfileView

---
+++ SwiftfullSourceControl/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SwiftfullSourceControl
+//
+//  Created by Mario Pereira on 5/27/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Nick!!!!!")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
